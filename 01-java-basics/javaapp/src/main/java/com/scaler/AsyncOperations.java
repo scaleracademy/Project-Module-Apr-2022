6a3f429a3be7694aed88ef7cd23765a4cb0e017c
Arnav Gupta <championswimmer@gmail.com>
2022-05-02 21:40:42 +0530

    update name in the MockLongTask class
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- 01-java-basics/javaapp/src/main/java/com/scaler/AsyncOperations.java
+++ 01-java-basics/javaapp/src/main/java/com/scaler/AsyncOperations.java
@@ -3,19 +3,29 @@ package com.scaler;
 public class AsyncOperations {
 
     static class MockLongTask {
+        private String name;
+        public MockLongTask(String name) {
+            this.name = name;
+        }
+
         void run () {
             new Thread(() -> {
-                System.out.println("MockLongTask.run() started");
+                System.out.println("MockLongTask.run(" + name + ") started");
                 var start = System.currentTimeMillis();
+                // fake 10 sec delay
+                // in real life, this would be a long running task
                 while (System.currentTimeMillis() - start < 10000) {}
-                System.out.println("MockLongTask.run() ended");
+                System.out.println("MockLongTask.run("+ name + ") ended");
 
             }).start();
         }
     }
 
     public static void main(String[] args) {
-        var task1 = new MockLongTask();
-        var task2 = new MockLongTask();
+        var task1 = new MockLongTask("download");
+        var task2 = new MockLongTask("encrypt");
+
+        task1.run();
+        task2.run();
     }
 }

fca8620a3333ed0321ddcc89b94a691b36e448ea
Arnav Gupta <championswimmer@gmail.com>
2022-05-09 21:41:52 +0530

    01: initial project setup with spring initializr
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ 02-spring-boot-basics/taskman-withdb/src/main/java/com/scaler/taskman/TaskmanWithdbApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.taskman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanWithdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanWithdbApplication.class, args);
+    }
+
+}
---
+++ 02-spring-boot-basics/taskman-withdb/src/test/java/com/scaler/taskman/TaskmanWithdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.taskman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanWithdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
